c85c6a928c48d896862da0f1c77491689b1f4374
Neisvestny <68970602+Neisvestny@users.noreply.github.com>
2024-12-17 20:40:04 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
 use rand::Rng;
 use std::io;
-use std::io::prelude::*;
+use std::io::Write;
 
 fn main() {
     println!("=== УГАДАЙ ЧИСЛО ===");
+    println!("Компьютер загадал число от 1 до 100. Попробуй угадать!\n");
 
     let rand_number = rand::thread_rng().gen_range(1..=100);
-    println!("Рандомное число: {rand_number}");
+    let mut attempts = 0;
 
-    let mut win = false;
-    while !win {
-        print!("Введи своё число: ");
-        io::stdout().flush().ok().expect("Error");
-        let mut input_number = String::new();
+    loop {
+        let input_number = match get_user_input() {
+            Some(num) => num,
+            None => continue,
+        };
 
-        io::stdin().read_line(&mut input_number).expect("Error");
-        let input_number: u32 = input_number.trim().parse().expect("Надо ввести число!");
+        attempts += 1;
 
-        println!("Твоё число: {input_number}");
-        if rand_number > input_number {
-            println!("Я загадал число, которое БОЛЬШЕ твоего")
-        } else if rand_number < input_number {
-            println!("Я загадал число, которое МЕНЬШЕ твоего")
+        if input_number < rand_number {
+            println!("Моё число БОЛЬШЕ твоего.\n");
+        } else if input_number > rand_number {
+            println!("Моё число МЕНЬШЕ твоего.\n");
         } else {
-            println!("\nТЫ ПОБЕДИЛ!!!");
-            win = !win
+            println!("\n🎉 ТЫ ПОБЕДИЛ!!! 🎉");
+            println!("Ты угадал число за {attempts} попыток.");
+            break;
+        }
+    }
+}
+
+fn get_user_input() -> Option<u32> {
+    print!("Введи своё число: ");
+    io::stdout().flush().expect("Ошибка при очистке буфера.");
+
+    let mut input = String::new();
+    if let Err(_) = io::stdin().read_line(&mut input) {
+        println!("Ошибка при вводе. Попробуй снова.");
+        return None;
+    }
+
+    match input.trim().parse::<u32>() {
+        Ok(num) => Some(num),
+        Err(_) => {
+            println!("Пожалуйста, введи корректное число!");
+            None
         }
     }
 }
